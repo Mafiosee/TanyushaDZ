5b4ffecf8ac0909f3376579c4035e9cdec288cd1
Mafiosee <135921977+Mafiosee@users.noreply.github.com>
2024-08-26 19:18:56 +0300

    Add files via upload

---
+++ TanyushaDZ2/TanyushaDZ2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+
+bool foo(int num)
+{
+    return num%2 == 0;
+}
+
+void func()
+{
+    int num;
+
+    std::cout << "ДЗ №2 :)\n";
+    std::cout << "Введіть будь-ласка число: ";
+    std::cin >> num;
+    if (foo(num)) std::cout << "Число парне :)\n";
+    else std::cout << "Число непарне :)\n";
+}
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    
+    while (true)
+    {
+        func();
+        Sleep(1000);
+    }
+}
