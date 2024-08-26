cf6f7f5c32352bb5b5c27bd2acc84bb6ce898231
Mafiosee <135921977+Mafiosee@users.noreply.github.com>
2024-08-26 19:19:25 +0300

    Add files via upload

---
+++ TanyushaDZ3/TanyushaDZ3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    std::string str;
+    std::cout << "ДЗ №3 : )\n" << "Введіть будь-ласка будь-яке слово : ";
+    std::cin >> str;
+    std::cout << "В слові '" + str + "' " << foo(str) << " голосних букв.\n";
+}
+
+std::string vowels = "АЕЄИІЇОУЮЯаеєиіїоуюя";
+
+int foo(std::string str)
+{
+    int result = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (std::find(vowels.begin(), vowels.end(), str[i]) == vowels.end())
+            continue;
+
+        result++;
+    }
+    return result;
+}
